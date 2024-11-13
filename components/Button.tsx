7c3e1517707eeca0069c0589341a4020e9e3ab70
Hamza El Idrissi <hamza.ezzharelidrissi1@gmail.com>
2024-11-13 15:07:00 +0100

    make submit the default type for buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({
-    type = 'button',
+    type = 'submit',
     className = '',
     ...props
 }) => (
